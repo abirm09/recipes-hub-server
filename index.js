7857dc948f285135885a07f60a51bcaed5571e44
Md. Abir Mahmud <abirmahmud5665@gmail.com>
2023-05-01 23:35:44 +0600

    Created all chefs API

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.port || 5000;
+const chefsData = require("./data/chefs-and-recipes.json");
 app.use(cors());
 
 app.get("/", (req, res) => {
   res.send(["Server is running."]);
+});
+
+//get all chefs data
+app.get("/chefs/all", (req, res) => {
+  res.send(chefsData);
 });
 
 app.listen(port, () => {
