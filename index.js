28d77f32405d1f7a2c895842369d8085a1fe925d
Md. Abir Mahmud <abirmahmud5665@gmail.com>
2023-05-01 23:31:06 +0600

    Server created successfully

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.port || 5000;
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send(["Server is running."]);
+});
+
+app.listen(port, () => {
+  console.log(`Server started at ${port}`);
+});
